039b843db6fe8190d1d0236f9f5f43e758828199
Martheo <theofanousmario@gmail.com>
2024-07-10 01:54:46 +0200

    seaborn

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import numpy as np
 import cv2
